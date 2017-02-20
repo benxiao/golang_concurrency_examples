1ddf57dc6ad300bc396359ecb3d2cd421ff6d918
ran xiao <ranxiao@192-168-1-5.tpgi.com.au>
2017-02-20 12:01:14 +1100

    add fake search engine

--- search_engine/main.go
+++ search_engine/main.go
@@ -1,4 +1,44 @@
 package main
 
+import (
+	"time"
+	"math/rand"
+	"fmt"
+)
+
 func main() {
+	rand.Seed(time.Now().UnixNano())
+	start := time.Now()
+	results := Google("golang")
+	elapsed := time.Since(start)
+	fmt.Println(results)
+	fmt.Println(elapsed)
+
+}
+
+var (
+	Web = fakeSearch("web")
+	Image = fakeSearch("image")
+	Video = fakeSearch("video")
+)
+
+type Result struct {
+	str string
+}
+
+type Search func(query string) Result
+
+func fakeSearch(kind string) Search {
+	return func(query string) Result {
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+		return Result{fmt.Sprintf("%s result for %q\n", kind, query)}
+	}
+}
+
+func Google(query string) []Result {
+	results := make([]Result, 0, 3)
+	results = append(results, Web(query))
+	results = append(results, Image(query))
+	results = append(results, Video(query))
+	return results
 }
